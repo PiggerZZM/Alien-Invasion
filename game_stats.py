8925f8f24c85e56738b90dfd36904a084c4e4680
PiggerZZM <442823793@qq.com>
2020-01-09 17:01:50 +0800

    添加最高分

--- game_stats.py
+++ game_stats.py
@@ -8,6 +8,9 @@ class GameStats():
         self.game_active = False
         self.reset_stats()
 
+        # 在任何情况下都不应重置最高得分
+        self.high_score = 0
+
     def reset_stats(self):
         """初始化在游戏运行期间可能变化的统计信息"""
         self.ships_left = self.ai_settings.ship_limit
--- scoreboard.py
+++ scoreboard.py
@@ -15,8 +15,10 @@ class Scoreboard():
         self.text_color = (30, 30, 30)
         self.font = pygame.ftfont.SysFont(None, 40)
 
-        # 准备初始得分图象
+        # 准备包含最高得分和当前得分的图象
         self.prep_score()
+        self.prep_high_score()
+
 
     def prep_score(self):
         """将得分转换为一副渲染的图象"""
@@ -35,3 +37,16 @@ class Scoreboard():
     def show_score(self):
         """在屏幕上显示得分"""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+
+    def prep_high_score(self):
+        """将最高得分转换为渲染的图象"""
+        high_score = int(round(self.stats.high_score, -1))
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(high_score_str, True,
+            self.text_color, self.ai_settings.bg_color)
+
+        # 将最高得分放在屏幕顶部中央
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
