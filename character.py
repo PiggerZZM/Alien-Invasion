a1c8f9938161d220b919aba52d6678436263c353
PiggerZZM <442823793@qq.com>
2020-01-10 18:02:43 +0800

    修改局部变量为类成员变量

--- character.py
+++ character.py
@@ -13,9 +13,9 @@ class Character():
     def draw_character(self):
         # 这里要注意.blit()方法的实现原理，第一个参数是Surface对象，表示要绘制的对象
         # 第二个参数是rect对象，绘制时取rect对象的左上角坐标作为Surface对象的左上角坐标
-        screen_rect = self.screen.get_rect()
-        image_rect = self.image.get_rect()
-        image_rect.centerx = screen_rect.centerx
-        image_rect.centery = screen_rect.centery
+        self.screen_rect = self.screen.get_rect()
+        self.image_rect = self.image.get_rect()
+        self.image_rect.centerx = self.screen_rect.centerx
+        self.image_rect.centery = self.screen_rect.centery
 
-        self.screen.blit(self.image, image_rect)
+        self.screen.blit(self.image, self.image_rect)
